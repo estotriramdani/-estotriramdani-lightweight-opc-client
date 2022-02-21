afddb1fde9b31e342246da8108aca649d21519ad
Esto Triramdani N <estolagi@gmail.com>
2022-02-21 14:06:58 +0700

    multi nodeId insert

---
+++ opc-client/multiNodeId.js
@@ -0,0 +1,165 @@
+const chalk = require('chalk');
+const {
+  OPCUAClient,
+  MessageSecurityMode,
+  SecurityPolicy,
+  AttributeIds,
+  ClientSubscription,
+  TimestampsToReturn,
+  ClientMonitoredItem,
+} = require('node-opcua');
+const insertToTableWithField = require('../database/insertToTableWithField');
+const { logger, getToday } = require('../helpers');
+
+async function multiNodeId({
+  endpointUrl,
+  nodeIds = [{ nodeId: '', fieldName: '' }],
+  infinite = false,
+  applicationName = 'MyApp',
+  databaseConfigs = {
+    host: '',
+    user: '',
+    password: '9121h23hui12h31jk23hjk12',
+    database: '',
+  },
+  tableName = '',
+  timestampField = '',
+  monitorTime = 10 * 1000,
+  isInsertToDatabase = false,
+}) {
+  if (!endpointUrl) {
+    throw Error('Please provide endpointUrl and nodeId');
+  }
+  const connectionStrategy = {
+    initialDelay: 1000,
+    maxRetry: 1,
+  };
+  const options = {
+    applicationName,
+    connectionStrategy: connectionStrategy,
+    securityMode: MessageSecurityMode.None,
+    securityPolicy: SecurityPolicy.None,
+    endpointMustExist: true,
+  };
+
+  const client = OPCUAClient.create(options);
+  await client.connect(endpointUrl);
+  console.log(chalk.cyan('Connected to OPC!'));
+
+  const session = await client.createSession();
+  console.log('session created!');
+
+  const browseResult = await session.browse('RootFolder');
+
+  console.log('references of RootFolder :');
+  for (const reference of browseResult.references) {
+    console.log('   -> ', reference.browseName.toString());
+  }
+  nodeIds.forEach(async ({ nodeId, fieldName }) => {
+    const value = 0;
+    const nodeToRead = {
+      nodeId,
+      attributeId: AttributeIds.Value,
+    };
+    const dataValue = await session.read(nodeToRead, value);
+    console.log('Value ', typeof dataValue.value);
+
+    const subscription = ClientSubscription.create(session, {
+      requestedPublishingInterval: 1000,
+      requestedLifetimeCount: 100,
+      requestedMaxKeepAliveCount: 10,
+      maxNotificationsPerPublish: 100,
+      publishingEnabled: true,
+      priority: 10,
+    });
+
+    subscription
+      .on('started', function () {
+        console.log(
+          'subscription started for 2 seconds - subscriptionId=',
+          subscription.subscriptionId
+        );
+        if (isInsertToDatabase) {
+          logger(chalk.bgGreen.white('The value will be stored to database.'));
+          logger(chalk.green(`Host => ${databaseConfigs.host}`));
+          logger(chalk.green(`Database => ${databaseConfigs.database}`));
+          logger(chalk.green(`Fieldname => ${fieldName}`));
+          logger(chalk.green(`Timestamp field => ${timestampField}`));
+          logger(chalk.green(`Table name => ${tableName}`));
+        }
+      })
+      .on('keepalive', function () {
+        console.log(
+          chalk.bgYellow.black('No value change occurs. Keep alive.')
+        );
+      })
+      .on('terminated', function () {
+        console.log('terminated');
+        return;
+      });
+
+    const itemToMonitor = {
+      nodeId,
+      attributeId: AttributeIds.Value,
+    };
+    const parameters = {
+      samplingInterval: 100,
+      discardOldest: true,
+      queueSize: 10,
+    };
+
+    const monitoredItem = ClientMonitoredItem.create(
+      subscription,
+      itemToMonitor,
+      parameters,
+      TimestampsToReturn.Both
+    );
+
+    monitoredItem.on('changed', async (dataValue) => {
+      console.log(
+        chalk.bgWhite.cyan(`[${getToday()}]`),
+        chalk.bgCyan.black(`New value: ${dataValue.value}`)
+      );
+      if (isInsertToDatabase) {
+        try {
+          const data = await insertToTableWithField({
+            databaseConfigs,
+            tableName,
+            fieldName,
+            value: dataValue.value.value,
+            timestampField,
+          });
+          console.log(data);
+          console.log(
+            chalk.bgGreen.white('Value was successfully inserted to database!')
+          );
+          console.log(
+            chalk.white('--------------------------------------------')
+          );
+        } catch (error) {
+          throw Error(error);
+        }
+      }
+    });
+
+    if (!infinite) {
+      async function timeout(ms) {
+        return new Promise((resolve) => setTimeout(resolve, ms));
+      }
+      await timeout(monitorTime);
+
+      console.log('now terminating subscription');
+      await subscription.terminate();
+
+      // close session
+      await session.close();
+
+      // disconnecting
+      await client.disconnect();
+      console.log('done !');
+      return;
+    }
+  });
+}
+
+module.exports = multiNodeId;
